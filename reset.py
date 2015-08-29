8a89aa06d0bb415f102ef922ef4f2e95fc5eaefa
panciunio <pancio@pancio.net>
2015-08-29 15:00:42 +0200

    Create reset.py

---
+++ reset.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# TM1638 playground
+
+import SUNXI_GPIO as GPIO
+import TM1638
+import time
+
+# These are the pins the display is connected to. Adjust accordingly.
+# In addition to these you need to connect to 5V and ground.
+
+DIO = GPIO.PG0
+CLK = GPIO.PG1
+STB = GPIO.PG2
+
+display = TM1638.TM1638(DIO, CLK, STB)
+
+display.enable(1)
+
+last = 6
+for j in range(1, 10):
+    for i in range(6):
+        display.send_char(last, 0x00)
+        display.send_char(i, 128 >> j-1)
+        last = i
+        time.sleep(0.05)
+
+display.set_text('')
+
+
